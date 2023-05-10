0351dc676ca4af6e3eff4125b1e77b8eb993bd8a
JakovSijarto <jakov.sijarto2@gmail.com>
2023-05-10 20:08:15 +0200

    Small improvements

--- src/assets/home.vue
+++ src/assets/home.vue
@@ -1,99 +1,117 @@
 <template>
     <div class="h-[200em] w-full">
-            <div class="fixed left-0 top-0 h-full w-[275px] pt-[60px] px-[70px] pb-[10px] flex flex-col justify-between" style="box-shadow: 1px 2px 8px 0 rgba(0,0,0,.06);">
+        <div class="fixed left-0 top-0 h-full w-[275px] pt-[60px] px-[70px] pb-[10px] flex flex-col justify-between max-[1000px]:hidden" style="box-shadow: 1px 2px 8px 0 rgba(0,0,0,.06);">
             <img src="../../public/logo.png" alt="" class="rounded-[50%]">
             <div class="font-bold text-[17px] leading-5 uppercase">
                 <p class="py-[5px]">Home</p>
                 <p class="py-[5px]">Portfolio</p>
             </div>
-            <div class="mb-[36px]">ikonica instagrama</div>
+            <div class="mb-[36px] flex flex-row">
+                <a href="https://www.instagram.com/tenisice_online/"><img src="../../public/ig_icon.svg" alt="" class="w-[24px] h-[24px]"></a>
+                <a href="https://www.facebook.com/marketplace/item/758450665833123"><img src="../../public/facebook_icon.svg" alt="" class="w-[18px] h-[24px] ml-4"></a>
+            </div>
         </div>
-        <div class="parent pl-[275px] w-full place-items-center pt-[103px]">
-            <div class="w-[80%]">
-                <div class="text-center">
-                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
-                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+        <div class="fixed top-0 z-101 bg-white w-full h-[70px] min-[1001px]:hidden flex justify-center items-center" style="box-shadow: 1px 2px 8px 0 rgba(0,0,0,.06);">
+            <div class="w-full mx-[5rem] flex items-center">
+                <label class="burger" for="burger">
+                    <input type="checkbox" id="burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
+                <div class="w-full flex justify-center">
+                    Tenisice Online
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+        </div>
+        <div class="parent pl-[275px] w-full place-items-center pt-[103px] max-[1000px]:pl-0">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150a</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect ">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
-            <div class="w-[80%]">
-                <div class="text-center">
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
+                </div>
+            </div>
+            <div class="w-[80%] m-8">
+                <div class="text-center hover_effect">
                     <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
                     <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsuma</p>
-                    <p class="text-[#464646] tag_price">$150</p>
+                    <p><a href="#" id="style-2" class="a1" data-replace="view more"><span class="text-[#464646] tag_price">150$</span></a></p>
                 </div>
             </div>
+            
         </div>
+        
     </div>
 </template>
 
@@ -114,5 +132,108 @@ grid-template-columns: repeat(3, 1fr);
 grid-template-rows: repeat(6, 1fr);
 grid-column-gap: 0px;
 grid-row-gap: 0px;
+}
+
+.a1 {
+  overflow: hidden;
+  position: relative;
+  z-index:-1;
+  display: inline-block;
+  font-family: 'Titillium Web', sans-serif;
+}
+
+.a1::before,
+.a1::after {
+ content: '';
+  position: absolute;
+  width: 100%;
+  left: 0;
+}
+
+.a1::after {
+  content: attr(data-replace);
+  height: 100%;
+  width:100%;
+  top: 0;
+  transform-origin: 100% 50%;
+  transform: translate3d(200%, 0, 0);
+  transition: transform .3s cubic-bezier(0.76, 0, 0.24, 1);
+  color: #464646;
+}
+
+.hover_effect:hover .a1::before {
+  transform-origin: 0% 50%;
+  transform: scaleX(1);
+}
+.hover_effect:hover .a1::after {
+  transform: translate3d(0, 0, 0);
+}
+
+.a1 span {
+  display: inline-block;
+  transition: transform .3s cubic-bezier(0.76, 0, 0.24, 1);
+}
+
+.hover_effect:hover .a1 span {
+  transform: translate3d(-200%, 0, 0);
+}
+.burger {
+  position: relative;
+  width: 20px;
+  height: 20px;
+  background: transparent;
+  cursor: pointer;
+  display: block;
+}
+
+.burger input {
+  display: none;
+}
+
+.burger span {
+  display: block;
+  position: absolute;
+  height: 4px;
+  width: 100%;
+  background: black;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  transform: rotate(0deg);
+  transition: .25s ease-in-out;
+}
+
+.burger span:nth-of-type(1) {
+  top: 0px;
+  transform-origin: left center;
+}
+
+.burger span:nth-of-type(2) {
+  top: 50%;
+  transform: translateY(-50%);
+  transform-origin: left center;
+}
+
+.burger span:nth-of-type(3) {
+  top: 100%;
+  transform-origin: left center;
+  transform: translateY(-100%);
+}
+
+.burger input:checked ~ span:nth-of-type(1) {
+  transform: rotate(45deg);
+  top: 0px;
+  left: 0px;
+}
+
+.burger input:checked ~ span:nth-of-type(2) {
+  width: 0%;
+  opacity: 0;
+}
+
+.burger input:checked ~ span:nth-of-type(3) {
+  transform: rotate(-45deg);
+  top: 14px;
+  left: 0px;
 }
 </style>
