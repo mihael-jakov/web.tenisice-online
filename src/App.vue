99c0eaeafe024bfa22f92afea5e2d096e55b58bd
JakovSijarto <jakov.sijarto2@gmail.com>
2023-05-10 13:31:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import home from "./assets/home.vue"
+</script>
+
+<template>
+<div>
+  <home/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/assets/home.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="h-[200em] w-full">
+            <div class="fixed left-0 top-0 h-full w-[275px] pt-[60px] px-[70px] pb-[10px] flex flex-col justify-between" style="box-shadow: 1px 2px 8px 0 rgba(0,0,0,.06);">
+            <img src="../../public/logo.png" alt="" class="rounded-[50%]">
+            <div class="font-bold text-[17px] leading-5 uppercase">
+                <p class="py-[5px]">Home</p>
+                <p class="py-[5px]">Portfolio</p>
+            </div>
+            <div class="mb-[36px]">ikonica instagrama</div>
+        </div>
+        <div class="parent pl-[275px] w-full place-items-center pt-[103px]">
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+            <div class="w-[80%]">
+                <div class="text-center">
+                    <img src="../../public/tenisice_primjer1.jpg" class="w-[100%]" alt="">
+                    <p class="mt-[4px] uppercase font-size-[17px] font-bold leading-tight ">Lorem Ipsum</p>
+                    <p class="text-[#464646] tag_price">$150</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400&display=swap');
+
+.tag_price{
+    font-family: 'Titillium Web', sans-serif;
+}
+
+.parent {
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: repeat(6, 1fr);
+grid-column-gap: 0px;
+grid-row-gap: 0px;
+}
+</style>
