6eb74d677280f58dcf20ffef21de7223073ff485
JakovSijarto <jakov.sijarto2@gmail.com>
2023-09-09 15:15:02 +0200

    aa

--- src/stores/dostupnoodmah.js
+++ src/stores/dostupnoodmah.js
@@ -6,7 +6,7 @@ export const dostupno_odmah = defineStore('products', {
     }),
     actions:{
         fetchProductsFromDB(){
-            fetch('../../public/dostupno_odmah.json')
+            fetch('../../dist/dostupno_odmah.json')
                 .then(res => res.json())
                 .then(json => 
                     this.products = json.products
